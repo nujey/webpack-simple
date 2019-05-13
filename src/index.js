e9259fd5e9043cd8110fb9dff2e8291aba412f95
张锋 <zhangfeng930101@qq.com>
2019-05-13 14:49:12 +0800

    输出管理

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import _ from 'lodash'
 import './assets/style/index.css'
 import cc from './assets/images/cc.jpeg'
+import printMe from './print'
 
 function component () {
   let element = document.createElement('div')
-
+  printMe()
   element.innerHTML = _.join(['Hola', 'webpack'], '~')
 
   var myImg = new Image()
