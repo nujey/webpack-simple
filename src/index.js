fd9afda41799f8f55fa76025b3b500c95d310042
张锋 <zhangfeng930101@qq.com>
2019-05-13 14:09:14 +0800

    管理资源部分

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import _ from 'lodash'
 import './assets/style/index.css'
+import cc from './assets/images/cc.jpeg'
 
 function component () {
   let element = document.createElement('div')
 
   element.innerHTML = _.join(['Hola', 'webpack'], '~')
+
+  var myImg = new Image()
+  myImg.src = cc
+  element.appendChild(myImg)
 
   return element
 }
--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,44 @@ module.exports = {
     rules: [{
       test: /\.css$/,
       use: ['style-loader', 'css-loader']
+    }, {
+      test: /\.(png|svg|jpe?g|gif)$/,
+      use: [
+        'file-loader',
+        // {
+        //   loader: 'url-loader',
+        //   options: {
+        //     limit: 8192,
+        //     name: 'images/[name]-[hash:8].[ext]',
+        //     quality: 80
+        //   }
+        // },
+        {
+          loader: 'image-webpack-loader',
+          options: {
+            mozjpeg: {
+              progressive: true,
+              quality: 65
+            },
+            optipng: {
+              enabled: false
+            },
+            pngquant: {
+              quality: '65-90',
+              speed: 4
+            },
+            gifsicle: {
+              interlaced: false
+            },
+            webp: {
+              quality: 75
+            }
+          }
+        }
+      ]
+    }, {
+      test: /\.(woff|woff2|eot|ttf|otf)$/,
+      use: ['file-loader']
     }]
   }
 }
