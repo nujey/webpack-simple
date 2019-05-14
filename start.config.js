30327fbc76575d22ba2d374337ca81707356ea15
张锋 <zhangfeng930101@qq.com>
2019-05-14 09:20:17 +0800

    生产环境和开发环境common配置

--- start.config.js
+++ start.config.js
@@ -1,21 +1,21 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
+const webpack = require('webpack')
 
 module.exports = {
+  mode: 'development',
   entry: {
-    app: './src/index.js',
-    print: './src/print.js'
+    app: './src/index.js'
   },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      title: '管理输出'
-    })
-  ],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+    hot: true
   },
   module: {
     rules: [{
@@ -60,5 +60,12 @@ module.exports = {
       test: /\.(woff|woff2|eot|ttf|otf)$/,
       use: ['file-loader']
     }]
-  }
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: '管理输出'
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ],
 }
