481a480d4805b9679e94e5fb31b15989e911b9c0
张锋 <zhangfeng930101@qq.com>
2019-05-16 09:32:09 +0800

    s

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import _ from 'lodash'
 import './assets/style/index.css'
 import cc from './assets/images/cc.jpeg'
-import Print from './print'
+// import Print from './print'
 
 if(process.env.NODE_ENV !== 'production') {
   console.log('Looks like we are in development mode!')
@@ -10,7 +10,7 @@ if(process.env.NODE_ENV !== 'production') {
 function component () {
   let element = document.createElement('div')
   element.innerHTML = _.join(['Hola', 'webpack'], '~')
-  Print()
+  // Print()
   var myImg = new Image()
   myImg.src = cc
   element.appendChild(myImg)
