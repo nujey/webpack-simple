8b2003f1609e200830a579ab548c29c6926a9528
张锋 <zhangfeng930101@qq.com>
2019-05-22 14:45:48 +0800

    ng new project

--- ng-app/src/app/app.component.ts
+++ ng-app/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'ng-app';
+  title = '我的angulardemo';
 }
--- ng-app/src/main.ts
+++ ng-app/src/main.ts
@@ -1,4 +1,6 @@
+// 开发者配置
 import { enableProdMode } from '@angular/core';
+// 
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
