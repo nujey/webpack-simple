c3ba44ab84e6359d194b7536c80a317270451954
张锋 <zhangfeng930101@qq.com>
2019-08-05 17:12:29 +0800

    自定义事件

--- ng-app/src/app/grammar-study/grammar-study.component.ts
+++ ng-app/src/app/grammar-study/grammar-study.component.ts
@@ -1,17 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, Input } from '@angular/core';
+import { EventEmitter } from 'events';
 
 @Component({
   selector: 'app-grammar-study',
   templateUrl: './grammar-study.component.html',
   styleUrls: ['./grammar-study.component.scss']
 })
+
 export class GrammarStudyComponent implements OnInit {
+  // 自定义时间
+  @Output() deleteRequest = new EventEmitter<Item>()
+  // 双向绑定的
+  @Input() size: number | string
+  @Output() sizeChange = new EventEmitter<number>()
 
   constructor() { }
   canSave: true
   isSpecial: false
   currentClasses: { }
   currentStyles: {}
+
   ngOnInit() {
   }
   deleteHero(): void {
@@ -27,5 +35,19 @@ export class GrammarStudyComponent implements OnInit {
     this.currentStyles = {
       'font-size': this.isSpecial ? '24px' : '12px'
     }
+  }
+  delete() {
+    this.deleteRequest.emit(this.item)
+  }
+
+  dec() {
+    this.resize(-1)
+  }
+  inc() {
+    this.resize(+1)
+  }
+  resize(delta: number) {
+    this.size = Math.min(40, Math.max(8, +this.size + delta))
+    this.sizeChange.emit(this.size)
   }
 }
