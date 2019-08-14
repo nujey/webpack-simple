2d75dd52337bdd4b01edad9e2ab6a5531dfdb00b
张锋 <zhangfeng930101@qq.com>
2019-08-14 17:43:15 +0800

    s

--- ng-app/src/app/app-eventemit/app-eventemit.component.ts
+++ ng-app/src/app/app-eventemit/app-eventemit.component.ts
@@ -3,8 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-app-eventemit',
   templateUrl: './app-eventemit.component.html',
-  styleUrls: ['./app-eventemit.component.scss']
+  styleUrls: ['./app-eventemit.component.scss'],
+  inputs: ['hero'], // 在组件的指令元数据中标记这些成员
+  outputs: ['deleteRequest']
 })
+
 export class AppEventemitComponent implements OnInit {
 
   constructor() { }
